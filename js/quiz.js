fcefcb0fef600d4b26a01aa66fddb91fa74b4b01
gaganjot99 <73100470+gaganjot99@users.noreply.github.com>
2020-11-01 10:20:05 +0530

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,86 @@
+let quiz = [
+    {
+        question: "Who wrote the famous book-'We the people'?",
+        option: [
+            "T.N.Kaul",
+            "J.R.D. Tata",
+            "Khushwant Singh",
+            "Nani Palkhivala",
+        ],
+        answer: 4,
+    },
+    {
+        question: "Who is the auther of the book 'Nineteen Eighty Four'?",
+        option: [
+            "Thomas Hardy",
+            "Emile Zola",
+            "George Orwell",
+            "Walter Scott",
+        ],
+        answer: 3,
+    },
+    {
+        question: "Which of the following is NOT written by Munshi Premchand?",
+        option: [
+            "Gaban",
+            "Godan",
+            "Guide",
+            "Manasorovar",
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            "Who is the auther of the book 'Forbidden Verses'?",
+        option: [
+            "Salman RushDie",
+            "Abu Nuwas",
+            "Ms. Taslima Nasrin",
+            "D.H. Lawrence",
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            "Who has written the book 'My Frozen Turbulence in Kashmir'?",
+        option: [
+            "Anees Jung",
+            "Jagmohan",
+            "M.J.Akbar",
+            "Nissim Ezekiel",
+        ],
+        answer: 2,
+    },
+    {
+        question: "The book 'To Live or Not Live' was written by",
+        option: ["V.S. Naipaul", "Alexandra Dumas", "George Elliot", "Nirad C. Chaudhuri"],
+        answer: 4,
+    },
+    {
+        question: "'A Voice for Freedom' is a book written by",
+        option: [
+            "Corazon Aquino",
+            "Nayantara Sahgal",
+            "Aung San Suu Kyi",
+            "Benazir Bhutto",
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            "Who is the father of English Poetry?",
+        option: ["Milton", "Wordsworth", "Chauser", "Charles Dickens"],
+        answer: 3,
+    },
+    {
+        question: "Which of the book was written by Graham Greene?",
+        option: ["O' Jerusalem", "The Power and Glory", "Crisis in India", "Of Human Bondage"],
+        answer: 2,
+    },
+    {
+        question: "George Bernard Shaw, the great dramatist, was",
+        option: ["a Welsh", "an Irishman", "a Soctsman", "an Englishman"],
+        answer: 2,
+    },
+];
 
